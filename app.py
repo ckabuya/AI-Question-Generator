54f526c90a634b94d6491e46457b189282b4c4b1
Chrispin Kabuya <chrispin.kabuya@gmail.com>
2024-06-22 00:17:50 +0200

    Update app.py to enable debugginz

--- app.py
+++ app.py
@@ -297,4 +297,5 @@ if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', debug=True, port=port)
+    
